8277e4975cd8f06b1e026a32397a89ec941cc352
BrokenFinger98 <dbtjsdn980818@gmail.com>
2025-10-19 02:29:07 +0900

    config(external): build.gradle.kts에 외부 api 테스트를 위한 mock 의존성 주입

--- modules/external/pg-client/build.gradle.kts
+++ modules/external/pg-client/build.gradle.kts
@@ -10,4 +10,9 @@ dependencies {
     implementation(projects.modules.application)
     implementation(projects.modules.domain)
     implementation(libs.spring.boot.starter.web)
+
+    testImplementation(libs.spring.boot.starter.test) {
+        exclude(module = "mockito-core")
+    }
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
 }
